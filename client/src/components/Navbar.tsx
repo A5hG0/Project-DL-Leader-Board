bb92d67f241b8d892185aef60169f22c99f428ed
A5hG0 <ashish522005@gmail.com>
2026-02-16 11:25:29 +0530

    added discord icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { FaDiscord } from "react-icons/fa";
 import "../styles/navbar.css";
 
 export default function Navbar() {
@@ -12,6 +13,16 @@ export default function Navbar() {
         <Link className={pathname === "/" ? "active" : ""} to="/">Home</Link>
         <Link className={pathname === "/leaderboard" ? "active" : ""} to="/leaderboard">Leaderboard</Link>
         <Link className={pathname === "/cases" ? "active" : ""} to="/cases">Case Studies</Link>
+
+        {/* Discord Icon */}
+        <a
+          href="https://discord.gg/E7huSCxRsW"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="discord-icon"
+        >
+          <FaDiscord size={20}/>
+        </a>
       </div>
     </nav>
   );
